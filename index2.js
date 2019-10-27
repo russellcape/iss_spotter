acfbb49dd8d19667769132d9ae37551df2fa48fd
Russell Cape <russellcape98@gmail.com>
2019-10-27 02:55:26 +0000

    Index (tests) for the promise method

---
+++ index2.js
@@ -0,0 +1,23 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./index')
+
+
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
+} = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
