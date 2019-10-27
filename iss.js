e33a9f73dfc3488c8f2b484ff23a2a17da188512
Russell Cape <russellcape98@gmail.com>
2019-10-27 00:54:15 +0000

    Fetches information by using the site ip address

--- iss.js
+++ iss.js
@@ -1,39 +1,51 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-
-
 const request = require('request');
 
-const fetchMyIP = function(callback) {
-  const url = 'https://api.ipify.org?format=json';
+// const fetchMyIP = function(callback) {
+//   const url = 'https://api.ipify.org?format=json';
+//   request(url, (error, resp, body) => {
+//     const data = JSON.parse(body);
+//     // console.log("error:" ,error)
+//     // console.log("data:", data)
+//     // console.log("data.ip:" ,data.ip)
+//     // console.log("typeof data:", typeof data)
+//     // console.log("typeof data.ip:", typeof data.ip)
+//     // console.log("typeof data.ip.toString():", typeof data.ip.toString())
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+//     if (resp.statusCode !== 200) {
+//       const msg = `Status Code ${resp.statusCode} when fetching IP. Response: ${data}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+//     if (data.length === 0) {
+//       callback(`Failed to find IP Address`, null);
+//     }
+//       callback(null, data.ip.toString());
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  const url = `https://ipvigilante.com/json/1.1${ip}`;
   request(url, (error, resp, body) => {
-    const data = JSON.parse(body);
-    // console.log("error:" ,error)
-    // console.log("data:", data)
-    // console.log("data.ip:" ,data.ip)
-    // console.log("typeof data:", typeof data)
-    // console.log("typeof data.ip:", typeof data.ip)
-    // console.log("typeof data.ip.toString():", typeof data.ip.toString())
     if (error) {
       callback(error, null);
       return;
     }
     if (resp.statusCode !== 200) {
-      const msg = `Status Code ${resp.statusCode} when fetching IP. Response: ${data}`;
-      callback(Error(msg), null);
+      callback(`Status Code ${resp.statusCode} when fetching IP. Response: ${body}`, null);
       return;
     }
-    if (data.length === 0) {
-      callback(`Failed to find IP Address`, null);
-    }  
-      callback(null, data.ip.toString());
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
   });
 };
 
-module.exports = { fetchMyIP };
+
+
+module.exports = {
+  //fetchMyIP,
+  fetchCoordsByIP
+};
