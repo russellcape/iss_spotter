9212108916d50725e46ec184f517c7019e55b419
Russell Cape <russellcape98@gmail.com>
2019-10-27 02:22:07 +0000

    Index uses function from iss.js and logs the web info

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-// const { fetchMyIP } = require('./iss');
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// const { fetchCoordsByIP } = require('./iss');
-
-// fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned Coordinates:' , coordinates);
-// });
-
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-const coordinates = { latitude: "37.38600", longitude: "-122.08380" };
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , ip);
+});
 
+fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned Coordinates:' , coordinates);
+});
+
+const coordinates = { latitude: '49.27670', longitude: '-123.13000' };
 fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
   console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  console.log(passTimes);
+
+  printPassTimes(passTimes)
 });
