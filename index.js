10a86c0d993b97d55ebc29683e2189e4322fc75b
Russell Cape <russellcape98@gmail.com>
2019-10-27 01:14:14 +0000

    Tested flyover time to ensure it worked

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned Coordinates:' , coordinates);
+// });
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const coordinates = { latitude: "37.38600", longitude: "-122.08380" };
+
+fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
